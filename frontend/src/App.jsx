ac2535ef3828e2b5d874e0b89e419177a01a7099
Sifat Bin Asad <sifatbinasad@gmail.com>
2024-07-08 15:55:34 +0600

    sda

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ import CompanySection from './component/user/company/CompanySection.jsx';
 import CompanyDetailsPage from './component/user/company/CompanyDetailsPage.jsx';
 import Login from './component/Login.jsx';
 import Registration from './component/Registration.jsx';
-
+//dsahuihajsdlaks
 import LayoutUser from './component/LayoutUser.jsx';
 import LayoutAdmin from './component/LayoutAdmin.jsx';
 import LayoutCompany from './component/LayoutCompany.jsx';
